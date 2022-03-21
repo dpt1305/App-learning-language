96d72c6cd3f039248ad757cd2b0dfe5699c40f60
tungjj <58741986+tungjj1@users.noreply.github.com>
2022-03-21 11:16:08 +0700

    UPDATE: *.json

--- ReactApp/App.js
+++ ReactApp/App.js
@@ -1,28 +1,50 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 
-import IntroScreen from "./src/screens/IntroScreen";
+import Tabs from "./src/navigation/Tabs"
+import LearnScreen from "./src/screens/LearnScreen"
+import OverviewScreen from "./src/screens/OverviewScreen"
+import LoginScreen1 from "./src/screens/LoginScreen1";
+import React from "react";
 
-import OnboardScreen from "./src/screens/OnboardScreen";
-import HomeScreen from "./src/screens/HomeScreen";
-import TestScreen from "./src/screens/TestScreen";
-import LoginScreen from "./src/screens/LoginScreen";
-import React, { useEffect } from "react";
-import AppIntroSlider from "react-native-app-intro-slider";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// import * as Google from "expo-google-app-auth";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// const isLogin = Google.isConnectAsync();
+// console.log(isLogin);
 
 export default function App() {
   return (
+    // <Stack.Navigator screenOptions={{ headerShown: false }}>
+    //   {/* <Stack.Screen name="LearnScreen" component={LearnScreen} /> */}
+    //   <Stack.Screen name="Login" component={LoginScreen} />
+
+    // </Stack.Navigator>
+    // <NavigationContainer>
+    //   <Tab.Navigator>
+    //     <Tab.Screen name="Account" component={LoginScreen} />
+    //     <Tab.Screen name="Learn" component={LearnScreen} />
+    //     <Tab.Screen name="Overview" component={OverviewScreen} />
+    //   </Tab.Navigator>
+    // </NavigationContainer>
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="OnboardScreen" component={OnboardScreen} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Navigator>
+        <Stack.Group>
+          <Stack.Screen name="Login1" component={LoginScreen1} />
+        </Stack.Group>
+        <Stack.Group>
+          <Stack.Screen name="Learn" component={LearnScreen} />
+        </Stack.Group>
       </Stack.Navigator>
+      {/* <Stack.Group>
+        <Stack.Screen />
+      </Stack.Group>
+      <Stack.Screen name="Tabs" component={Tabs} /> */}
     </NavigationContainer>
   );
 }
