b028f59c9fe2acd6534cfb9bf115e8513ee94b01
tungjj <tungplatin@gmail.com>
2022-06-20 06:09:23 +0700

    DONE: almost review screen

--- ReactApp/src/screens/overview/ReviewScreen.js
+++ ReactApp/src/screens/overview/ReviewScreen.js
@@ -15,53 +15,32 @@ import dataSlice from "../../redux/data.slice";
 import {MaterialIndicator} from "react-native-indicators";
 //# check box
 import CheckBox from 'react-native-check-box';
+import BlockMultiple from '../util/BlockMultiple';
 
 const window = Dimensions.get('window');
 
+function RenderBlock() {
+  return <BlockMultiple/>
+}
+
 export default function ReviewScreen(props) {
-  const answers = [
-    {id: 1, choice: 'A. ha'},
-    {id: 2, choice: 'B. i'},
-    {id: 3, choice: 'hb'},
-    {id: 4, choice: 'hc'},
-  ];
-  const renderItem =({item}) => (
-    <TouchableOpacity style={styles.buttonMultipleChoice}>
-      <Text style={{fontSize: 28,}}>{item.choice}</Text> 
-    </TouchableOpacity>
-  );
+  //# get data from redux
+  const buttonState = useSelector(buttonStateSelector);
+
   return (
     <SafeAreaView style={styles.container}>
       <BlockTopBar style={styles.topBar} navigation={props.navigation} />
 
-      <Text style={styles.title}>Choose the meaning of this word.</Text>
+      <View style={styles.viewBlock}>
+        <RenderBlock/>
 
-      <View style={styles.wordHeader}>
-        <Text style={{fontSize: 30,}}> lesson </Text>
-        <Text style={{fontSize: 30,}}>/ ˈles.ən /</Text>
-      </View>
-
-      <View style={styles.multipleChoice}>
-        {/* <FlatList
-          data={answers}
-          renderItem={renderItem}
-          showsVerticalScrollIndicator={false}
-          vertical={false}
-          contentContainerStyle={{
-            flex: 0,
-            marginLeft: "auto",
-            marginRight: "auto",
-          }}
-        /> */}
-        <TouchableOpacity style={styles.buttonMultipleChoice}>
-          <Text style={{fontSize: 28,}}>A</Text> 
-        </TouchableOpacity>
       </View>
 
       <View style={styles.buttonNextView}>
           <TouchableOpacity
+            style={buttonState ? styles.buttonNext : styles.buttonNextDisable}
             // style={disableButton ? styles.buttonNextDisable : styles.buttonNext}
-            // disabled={disableButton}
+            disabled={!buttonState}
             onPress={() => handleNextButton()}
           >
             <Text style={{ fontSize: 34, padding: 20 }}>Next</Text>
@@ -79,45 +58,27 @@ const styles = StyleSheet.create({
   topBar: {
     flex: 2,
   },
-  title: {
-    flex: 1,
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 10,
+  viewBlock: {
+    flex: 7
   },
-  wordHeader: {
-    flex: 1,
-    borderColor: config.primary,
-    borderWidth: 2,
-    borderRadius: 20,
-    marginRight: window.width/20,
-    marginLeft: window.width/20,
-    justifyContent:'center',
-    alignItems: 'center',
-  },
-  multipleChoice: {
-    flex: 5,
-    borderColor: config.primary,
-    // borderWidth: 2,
-    borderRadius: 20,
-    marginRight: window.width/20,
-    marginLeft: window.width/20,
-    marginTop: 30,
-    justifyContent:'center',
-    alignItems: 'center',
-  },
-  buttonMultipleChoice: {
-    borderColor: config.blockImage,
-    borderWidth: 3,
-    width: window.width/20*18,
-    height: window.height/20*1.3,
-    marginTop: 20,
-    borderRadius: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
   buttonNextView: {
     flex: 1,
+    alignItems: 'center',
   },
-
+  buttonNextDisable: {
+    width: 340,
+    backgroundColor: config.disable,
+    borderRadius: 20,
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: config.backgroundColor,
+  },
+  buttonNext: {
+    width: 340,
+    backgroundColor: config.primary,
+    borderRadius: 20,
+    alignItems: "center",
+    borderWidth: 1,
+  },
 })
---
+++ ReactApp/src/screens/util/BlockMultiple.js
@@ -0,0 +1,129 @@
+import { Alert, Dimensions, Modal, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+//# config
+import config from '../../config'
+import { useState } from 'react';
+
+const window = Dimensions.get('window');
+
+function Answer(props) {
+  const handleClick = () => {
+    console.log(props.answer.word, props.word.word );
+    if(props.answer.word == props.word.word) {
+      Alert.alert('Exactly');
+    }
+  };
+  return (
+    <TouchableOpacity 
+      style={styles.buttonMultipleChoice} 
+      onPress={() => {
+        handleClick();
+        // console.log(props.answer);
+      }}
+    >
+      <Text style={{fontSize: 20,}}>{props.answer.choice}</Text> 
+    </TouchableOpacity>
+  )
+}
+
+export default function BlockMultiple() {
+  //# data for answers
+  const answers = [
+    {id: 1, word: 'learn', choice: 'Study or something likes that.'},
+    {id: 2, word: 'student', choice: 'HA'},
+    {id: 3, word: 'school', choice: 'hb'},
+    {id: 4, word: 'lion', choice: 'hc'},
+  ];
+  //# exact word
+  const word = {
+    word: 'learn',
+    meaning: 'Study or something likes that.'
+  };
+  //# useState
+  const [modalVisible, setModalVisible] = useState(true);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Choose the meaning of this word.</Text>
+
+      <View style={styles.wordHeader}>
+        <Text style={{fontSize: 30,}}> lesson </Text>
+        <Text style={{fontSize: 30,}}>/ ˈles.ən /</Text>
+      </View>
+
+      <View style={styles.multipleChoice}>
+        <Answer answer={answers[0]} word={word}/>
+        <Answer answer={answers[1]} word={word}/>
+        <Answer answer={answers[2]} word={word}/>
+        <Answer answer={answers[3]} word={word}/>
+      </View>
+
+    {/* <View >
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}  
+      >
+        <View style={styles.centeredView}>
+          <Text>{word.word}</Text>
+          <Text>{word.meaning}</Text>
+        </View>
+      </Modal>
+    </View> */}
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    flex: 0.5,
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  wordHeader: {
+    flex: 1,
+    borderColor: config.primary,
+    borderWidth: 2,
+    borderRadius: 20,
+    marginRight: window.width/20,
+    marginLeft: window.width/20,
+    justifyContent:'center',
+    alignItems: 'center',
+  },
+  multipleChoice: {
+    flex: 5,
+    borderColor: config.primary,
+    // borderWidth: 2,
+    borderRadius: 20,
+    marginRight: window.width/20,
+    marginLeft: window.width/20,
+    marginTop: 30,
+    // justifyContent:'center',
+    alignItems: 'center',
+  },
+  buttonMultipleChoice: {
+    borderColor: config.blockImage,
+    borderWidth: 3,
+    width: window.width/20*18,
+    height: window.height/20*2.2,
+    marginTop: 10,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+    width: window.width/20*18,
+    height: window.height/20,
+    borderColor: 'red',
+    borderWidth: 2,
+  },
+})
